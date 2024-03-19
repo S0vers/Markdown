18827cf02184b7846bd15107942bd7aa45a28dec
s0vers <sovers.tonmoy.pandey@g.bracu.ac.bd>
2024-03-19 14:35:04 +0600

    file saved in json file

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
 import { ForwardRefEditor } from "./ForwardRedEditor";
 import "@mdxeditor/editor/style.css";
+import { MDXEditorMethods } from "@mdxeditor/editor";
 
 const EditorComp = dynamic(() => import("./EditorComponent"), { ssr: false });
 
@@ -16,12 +18,34 @@ const markdown = `
 `;
 
 export default function Home() {
+  const editorRef = useRef<MDXEditorMethods | null>(null);
+  const handleSave = async () => {
+    try {
+      const content = editorRef.current?.getMarkdown();
+      const response = await fetch("/api/save", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content }),
+      });
+      if (response.ok) {
+        console.log("Content saved successfully!");
+      } else {
+        console.error("Failed to save content:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error saving content:", error);
+    }
+  };
+
   return (
     <>
       <br />
       <div style={{ border: "1px solid black" }}>
         <Suspense fallback={null}>
-          <ForwardRefEditor markdown={markdown} />
+          <ForwardRefEditor markdown={markdown} ref={editorRef} />
+          <button onClick={handleSave}>Save</button>
         </Suspense>
       </div>
     </>
